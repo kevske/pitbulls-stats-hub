6a98648672c57e2118ec15d4c4b46a46735fd1db
Kevin Rassner <kevin.rassner@gmail.com>
2025-11-04 08:07:17 +0100

    Revert "sidebar improvements"
    This reverts commit 0e87358c60a8f45b5c861aea415d0f5bc65ce9dd.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Sidebar from "./components/Sidebar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
@@ -35,23 +34,18 @@ const App = () => {
         <TooltipProvider>
           <StatsProvider>
             <BrowserRouter basename="/pitbulls-stats-hub">
-              <div className="flex min-h-screen">
-                <Sidebar />
-                <main className="flex-1 md:pl-64 transition-all duration-300">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/stats" element={<Stats />} />
-                    <Route path="/players" element={<Players />} />
-                    <Route path="/players/:id" element={<PlayerDetail />} />
-                    <Route path="/games" element={<Games />} />
-                    <Route path="/games/:id" element={<GameDetail />} />
-                    <Route path="/videos" element={<Videos />} />
-                    <Route path="/upload-game" element={<UploadGame />} />
-                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                  </Routes>
-                </main>
-              </div>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/stats" element={<Stats />} />
+                <Route path="/players" element={<Players />} />
+                <Route path="/players/:id" element={<PlayerDetail />} />
+                <Route path="/games" element={<Games />} />
+                <Route path="/games/:id" element={<GameDetail />} />
+                <Route path="/videos" element={<Videos />} />
+                <Route path="/upload-game" element={<UploadGame />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
             </BrowserRouter>
             <Toaster />
             <Sonner />
