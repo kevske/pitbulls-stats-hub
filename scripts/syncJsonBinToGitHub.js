5f6920127cb3d7f4549631bbf7be4bee6f3fe39c
Kevin Rassner <kevin.rassner@gmail.com>
2025-12-10 03:34:17 +0100

    fixed cron job

--- scripts/syncJsonBinToGitHub.js
+++ scripts/syncJsonBinToGitHub.js
@@ -1,5 +1,5 @@
-const fs = require('fs').promises;
-const path = require('path');
+import fs from 'fs/promises';
+import path from 'path';
 
 // Fetch all game data from JSONBin MasterBin
 async function syncJsonBinToGitHub() {
